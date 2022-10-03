7e37fe503c1bcd6ae7405f8071f8584b2d5266fa
Yan Latyshev <yan.latyshev@gmail.com>
2022-10-03 21:23:45 +0300

    create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React, { FC } from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "@components/Header/Header";
+import Footer from "@components/Footer/Footer";
+import AllProducts from "@pages/AllProducts/AllProducts";
+import Product from "@pages/Product/Product";
+import CheckOut from "@pages/CheckOut/CheckOut";
 import "./App.css";
 
 const App: FC = () => {
-  return <div className="App">Food Shop</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<AllProducts />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/checkout" element={<CheckOut />} />
+        <Route>404 not found</Route>
+      </Routes>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
